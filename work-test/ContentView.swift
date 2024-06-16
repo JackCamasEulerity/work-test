ab2f8608913971648054ae2cf3ced2140c43251f
Jack Camas <jackcamasdev@gmail.com>
2024-06-16 18:59:53 -0400

    Initial Commit

---
+++ work-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  work-test
+//
+//  Created by Jack Camas on 6/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ work-test/work_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  work_testApp.swift
+//  work-test
+//
+//  Created by Jack Camas on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct work_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
