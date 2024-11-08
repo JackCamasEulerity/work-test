c5aa52dfa492e84cbf5408953f1d30398535309e
Jack Camas <jack.camas@eulerity.com>
2024-11-08 16:00:18 -0500

    testtest

--- work-test/ContentView.swift
+++ work-test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("test1")
+            Text("testtest")
         }
         .padding()
     }
