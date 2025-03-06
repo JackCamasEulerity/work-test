10b0179b3c723236c58888d051fd0254f9ae8f2a
Jack Camas <jack.camas@eulerity.com>
2025-03-06 17:17:00 -0500

    Added missing tests

--- work-test/ContentView.swift
+++ work-test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("test2")
+            Text("test3")
         }
         .padding()
     }
